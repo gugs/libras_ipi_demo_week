b656492e310bbde69e536edad9bfff8a7c645778
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-26 16:04:11 -0300

    npm install

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' =>  '3306',
             'database' => 'forge',
             'username' =>  'root',
-            'password' =>  'root123',
+            'password' =>  'amanda123',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
